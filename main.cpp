40800ca344d610ca253990d7b2e5fccafe73d262
Svein Kåre <sveiks@stud.ntnu.no>
2024-05-22 10:18:31 +0200

    Code cleanup

--- main.cpp
+++ main.cpp
@@ -85,7 +85,6 @@ void runGpuDotProduct() {
         compute::reduce(device_result.begin(), device_result.end(), &result, queue);
     }
     auto end = high_resolution_clock::now();
-    cout << "result: " << result << endl;
     auto startTime = duration_cast<microseconds>(start.time_since_epoch());
     auto endTime = duration_cast<microseconds>(end.time_since_epoch());
     auto average = (endTime.count() - startTime.count()) / iterations;
@@ -146,30 +145,10 @@ void runThreadedMatrixMultiplication() {
 }
 
 int main() {
-    /* auto device = compute::system::default_device();
-    compute::context context(device);
-    compute::command_queue queue(context, device);
-    vector<int> a = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    vector<int> b = {0, 1, 2, 0, 1, 2, 0, 1, 2, 0};
-    vector<int> c(10);
-    compute::vector<int> device_a(a.size(), context);
-    compute::vector<int> device_b(b.size(), context);
-    compute::copy(a.begin(), a.end(), device_a.begin(), queue);
-    compute::copy(b.begin(), b.end(), device_b.begin(), queue);
-    compute::vector<int> device_c(c.size(), context);
-    compute::transform(device_a.begin(), device_a.end(),
-                       device_b.begin(), device_c.begin(), compute::plus<int>(), queue);
-    compute::copy(device_c.begin(), device_c.end(), c.begin(), queue);
-    cout << "c: " << endl;
-    for (int i = 0; i < 10; i++) {
-        cout << c[i] << " ";
-    } */
     runRegularDotProduct();
-    //runThreadedDotProduct();
-    //runGpuDotProduct();
-    //runRegularMatrixMultiplication();
-    //runThreadedMatrixMultiplication();
-    
-
+    runThreadedDotProduct();
+    runGpuDotProduct();
+    runRegularMatrixMultiplication();
+    runThreadedMatrixMultiplication();
     return 0;
 }
